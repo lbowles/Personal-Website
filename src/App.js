9dc742386cdcec3dddf84216f3de29ef1f45132a
lbowles <lspbowles@gmail.com>
2022-03-07 18:55:45 +0200

    base

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Intro from "./components/Intro.tsx"
+
+function App() {
+  return (
+    <div className="App">
+      <Intro></Intro>
+    </div>
+  );
+}
+
+export default App;
