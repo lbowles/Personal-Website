7c5f68448d72f0114624fb621fd78525ff34bd9b
lbowles <lspbowles@gmail.com>
2022-07-31 11:46:06 +0200

    updated experience

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,26 +1,31 @@
 import React from 'react'
-import UCT from "../../img/UCT.png"
+import UCT from '../../img/UCT.png'
 
 type Props = {}
 
 const Education = (props: Props) => {
   return (
     <div>
-        <h1>Education</h1>
-        <div id="out" className='rowContainer'>
-          <div>
-            <div id="point" className="point"></div>
-          </div>
-          <div>
-            <div className='rowContainer'>
-              <img src={UCT} style={{height:"18px",marginTop:"4px",paddingRight:"6px"}}></img>
-              <h2>University Of Cape Town</h2>
-            </div>
-            <h3 style={{marginTop:"-12px"}}>Bsc Computer Science and Business Computing</h3>
-            <h3 style={{marginTop:"0px",color:"#777777"}}>[2020-2023]</h3>
-          </div>
-          <div></div>
+      <h1>Education</h1>
+      <div id="out" className="rowContainer">
+        <div>
+          <div id="point" className="point"></div>
         </div>
+        <div>
+          <div className="rowContainer">
+            <img
+              src={UCT}
+              style={{ height: '18px', marginTop: '4px', paddingRight: '6px' }}
+            ></img>
+            <h2>University Of Cape Town</h2>
+          </div>
+          <h3 style={{ marginTop: '-12px' }}>
+            Bsc Computer Science and Business Computing
+          </h3>
+          <h3 style={{ marginTop: '0px', color: '#777777' }}>[2020-2023]</h3>
+        </div>
+        <div></div>
+      </div>
     </div>
   )
 }
--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,52 +1,115 @@
 import React from 'react'
-import style from "./Experience.module.css"
-import linkIcon from "../../img/link.svg"
-import neonLogo from "../../img/neon.png"
-import traqLogo from "../../img/traq.png"
-import klima from "../../img/klima.png"
-import punk from "../../img/punk.png"
-import clock from "../../img/clock.svg"
-import onset from "../../img/onset.svg"
-
-
-
+import style from './Experience.module.css'
+import linkIcon from '../../img/link.svg'
+import neonLogo from '../../img/neon.png'
+import traqLogo from '../../img/traq.png'
+import klima from '../../img/klima.png'
+import punk from '../../img/punk.png'
+import clock from '../../img/clock.svg'
+import onset from '../../img/onset.svg'
+import westernCape from '../../img/westernCape.jpeg'
 
 type Props = {}
 
 const Experience = (props: Props) => {
-  const projects = [{title:"Cell Traq",description:"System monitoring cathode efficiency in electrowinning plants.",date:"[2018-2019]",link:"https://traxs.tech/",color:"#8B90FE",logo:traqLogo},
-                    {title:"Neon Analytics",description:"LinkedIn marketing analytics and reporting",date:"[2020-2022]",link:"https://analytics.neonsolutions.co.za",color:"#BE8BFE",logo:neonLogo},
-                    {title:"Klima Incoom",description:"Simple staking rewards calculator for KlimaDAO",date:"[2021]",link:"https://klimaincoom.luko.co.za/",color:"#E08BFE",logo:klima},
-                    {title:"Synthetic Punks",description:"A unique, fully on-chain CryptoPunk for every Ethereum address",date:"[2022]",link:"https://syntheticpunks.com/",color:"#FE8BDE",logo:punk},
-                    {title:"Moment NFT",description:"A fully on-chain clock NFT",date:"[2022]",link:"https://momentNFT.luko.co.za/",color:"#FE8B92",logo:clock},
-                    {title:"Onset Cardon Retirement Verification",description:"Verification tool build for tracking on-chain carbon credit retirements",date:"[2022]",link:"https://www.onsetcarbon.com/",color:"#FEAD8B",logo:onset}
-                   ]
-                    
+  const projects = [
+    {
+      title: 'Cell Traq',
+      description:
+        'System monitoring cathode efficiency in electrowinning plants.',
+      date: '[2018-2019]',
+      link: 'https://traxs.tech/',
+      color: '#8B90FE',
+      logo: traqLogo,
+    },
+    {
+      title: 'Neon Analytics',
+      description: 'LinkedIn marketing analytics and reporting',
+      date: '[2020-2022]',
+      link: 'https://analytics.neonsolutions.co.za',
+      color: '#BE8BFE',
+      logo: neonLogo,
+    },
+    {
+      title: 'Klima Incoom',
+      description: 'Simple staking rewards calculator for KlimaDAO',
+      date: '[2021]',
+      link: 'https://klimaincoom.luko.co.za/',
+      color: '#E08BFE',
+      logo: klima,
+    },
+    {
+      title: 'Synthetic Punks',
+      description:
+        'A unique, fully on-chain CryptoPunk for every Ethereum address',
+      date: '[2022]',
+      link: 'https://syntheticpunks.com/',
+      color: '#FE8BDE',
+      logo: punk,
+    },
+    {
+      title: 'Moment NFT',
+      description: 'A fully on-chain clock NFT',
+      date: '[2022]',
+      link: 'https://momentNFT.luko.co.za/',
+      color: '#FE8B92',
+      logo: clock,
+    },
+    {
+      title: 'Onset Carbon',
+      description: 'Easy, publicly verifiable carbon offsetting',
+      date: '[2022]',
+      link: 'https://www.onsetcarbon.com/',
+      color: '#FEAD8B',
+      logo: onset,
+    },
+    {
+      title: 'Substance Use Disorder Assessment Tool',
+      description:
+        'Standardised tool for assessing substance use disorders in the Western Cape (South Africa)',
+      date: '[2022]',
+      color: '#fedc8b',
+      logo: westernCape,
+    },
+  ]
 
   return (
     <div>
-        <h1>Experience</h1>
-        {projects.map((project,index)=>(
-          <div id="out" className='rowContainer moveUp'  style={{paddingTop: index>0 ? "12px" : "5px",width:"100%"}} >
-            <div >
-              <div className="point" id="point"  style={{backgroundColor:project.color}}></div>
-            </div>
-            <div >
-              <div className='rowContainer'>
-                <img src={project.logo} className={style.logo}></img>
-                <h2>{project.title}</h2>
-              </div>
-              <h3 className={style.infoText}>{project.description}</h3>
-              <h3 className={style.date}>{project.date}</h3>
-            </div>
-            <div style={{float:"right",marginLeft:"auto",marginRight:'0'}}>
-              <a target="_blank" href={project.link}>
-                <img src={linkIcon} className={style.icon}></img>
-              </a>
-            </div>
+      <h1>Experience</h1>
+      {projects.map((project, index) => (
+        <div
+          id="out"
+          className="rowContainer moveUp"
+          style={{ paddingTop: index > 0 ? '12px' : '5px', width: '100%' }}
+        >
+          <div>
+            <div
+              className="point"
+              id="point"
+              style={{ backgroundColor: project.color }}
+            ></div>
           </div>
-        ))
-        }
+          <div>
+            <div className="rowContainer">
+              <img src={project.logo} className={style.logo}></img>
+              <h2>{project.title}</h2>
+            </div>
+            <h3 className={style.infoText}>{project.description}</h3>
+            <h3 className={style.date}>{project.date}</h3>
+          </div>
+          <div style={{ float: 'right', marginLeft: 'auto', marginRight: '0' }}>
+            {project.link ? (
+              <>
+                <a target="_blank" href={project.link}>
+                  <img src={linkIcon} className={style.icon}></img>
+                </a>
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
