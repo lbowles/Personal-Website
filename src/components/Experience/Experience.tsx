60f4a487dd98128a8972e0209a35523d323c2943
lbowles <lspbowles@gmail.com>
2022-12-30 14:34:18 +0200

    Added simple learning and solar

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,88 +1,108 @@
-import React from 'react'
-import style from './Experience.module.css'
-import linkIcon from '../../img/link.svg'
-import neonLogo from '../../img/neon.png'
-import traqLogo from '../../img/traq.png'
-import klima from '../../img/klima.png'
-import punk from '../../img/punk.png'
-import clock from '../../img/clock.svg'
-import onset from '../../img/onset.svg'
-import westernCape from '../../img/westernCape.svg'
-import walletUsage from '../../img/walletUsage.svg'
+import React from "react";
+import style from "./Experience.module.css";
+import linkIcon from "../../img/link.svg";
+import neonLogo from "../../img/neon.png";
+import traqLogo from "../../img/traq.png";
+import klima from "../../img/klima.png";
+import punk from "../../img/punk.png";
+import clock from "../../img/clock.svg";
+import onset from "../../img/onset.svg";
+import westernCape from "../../img/westernCape.svg";
+import walletUsage from "../../img/walletUsage.svg";
+import solarSystems from "../../img/solarSystems.svg";
+import simpleLearningTime from "../../img/simpleLearningTime.png";
 
-type Props = {}
+type Props = {};
 
 const Experience = (props: Props) => {
   const projects = [
     {
-      title: 'Cell Traq',
+      title: "Simple Learning Time",
       description:
-        'System monitoring cathode efficiency in electrowinning plants.',
-      date: '[2018-2019]',
-      link: 'https://masq.co.za/cell-traq/',
-      color: '#8B90FE',
+        "An educational app to teach students how to tell time. It is used in multiple schools across South Africa and several other countries.",
+      date: "[2016]",
+      link: "https://apps.apple.com/za/app/simple-learning-time/id1096092596",
+      color: "#8BABFE",
+      logo: simpleLearningTime,
+    },
+    {
+      title: "Cell Traq",
+      description:
+        "System monitoring cathode efficiency in electrowinning plants.",
+      date: "[2018-2019]",
+      link: "https://masq.co.za/cell-traq/",
+      color: "#8B90FE",
       logo: traqLogo,
     },
     {
-      title: 'Neon Analytics',
-      description: 'LinkedIn marketing analytics and reporting',
-      date: '[2020-2022]',
-      link: 'https://analytics.neonsolutions.co.za',
-      color: '#BE8BFE',
+      title: "Neon Analytics",
+      description: "LinkedIn marketing analytics and reporting",
+      date: "[2020-2022]",
+      link: "https://analytics.neonsolutions.co.za",
+      color: "#BE8BFE",
       logo: neonLogo,
     },
     {
-      title: 'Klima Incoom',
-      description: 'Simple staking rewards calculator for KlimaDAO',
-      date: '[2021]',
-      link: 'https://klimaincoom.luko.co.za/',
-      color: '#E08BFE',
+      title: "Klima Incoom",
+      description: "Simple staking rewards calculator for KlimaDAO",
+      date: "[2021]",
+      link: "https://klimaincoom.luko.co.za/",
+      color: "#E08BFE",
       logo: klima,
     },
     {
-      title: 'Synthetic Punks',
+      title: "Synthetic Punks",
       description:
-        'A unique, fully on-chain CryptoPunk for every Ethereum address',
-      date: '[2022]',
-      link: 'https://syntheticpunks.com/',
-      color: '#FE8BDE',
+        "A unique, fully on-chain CryptoPunk for every Ethereum address",
+      date: "[2022]",
+      link: "https://syntheticpunks.com/",
+      color: "#FE8BDE",
       logo: punk,
     },
     {
-      title: 'Moment NFT',
-      description: 'A fully on-chain clock NFT',
-      date: '[2022]',
-      link: 'https://momentNFT.luko.co.za/',
-      color: '#FE8B92',
+      title: "Moment NFT",
+      description: "A fully on-chain clock NFT",
+      date: "[2022]",
+      link: "https://momentNFT.luko.co.za/",
+      color: "#FE8B92",
       logo: clock,
     },
-        {
-      title: 'Substance Use Disorder Assessment Tool',
+    {
+      title: "Substance Use Disorder Assessment Tool",
       description:
-        'Standardised tool for assessing substance use disorders in the Western Cape (South Africa)',
-      date: '[2022]',
-      color: '#FEAD8B',
-      link:'https://thundering-morning-845.notion.site/WC-SUDAT-User-Manual-fbec5295cae441e1a0e5779a79e536f7',
+        "Standardised tool for assessing substance use disorders in the Western Cape (South Africa) - Final year Comp Sci project",
+      date: "[2022]",
+      color: "#FEAD8B",
+      link: "https://thundering-morning-845.notion.site/WC-SUDAT-User-Manual-fbec5295cae441e1a0e5779a79e536f7",
       logo: westernCape,
     },
-        {
-      title: 'Wallet Usage',
+    {
+      title: "Wallet Usage",
       description:
-        'Simple tool showing wallet activity, made to test development using ChatGPT',
-      date: '[2022]',
-      link: 'https://walletusage.luko.co.za/',
-      color: '#fedc8b',
+        "Simple tool showing wallet activity, made to test development using ChatGPT",
+      date: "[2022]",
+      link: "https://walletusage.luko.co.za/",
+      color: "#fedc8b",
       logo: walletUsage,
     },
     {
-      title: 'Onset Carbon',
-      description: 'Easy, publicly verifiable carbon offsetting',
-      date: '[2022]',
-      link: 'https://www.onsetcarbon.com/',
-      color: '#f3fe8b',
+      title: "On-chain Solar Systems",
+      description:
+        "Fully on-chain, procedurally generated, animated solar systems. 100+ Eth trading volume",
+      date: "[2022]",
+      color: "#F8F496",
+      link: "https://onchainsolar.systems/",
+      logo: solarSystems,
+    },
+    {
+      title: "Onset Carbon",
+      description: "Easy, publicly verifiable carbon offsetting",
+      date: "[2022-ongoing]",
+      link: "https://www.onsetcarbon.com/",
+      color: "#D9F896",
       logo: onset,
     },
-  ]
+  ];
 
   return (
     <div>
@@ -91,7 +111,7 @@ const Experience = (props: Props) => {
         <div
           id="out"
           className="rowContainer moveUp"
-          style={{ paddingTop: index > 0 ? '12px' : '5px', width: '100%' }}
+          style={{ paddingTop: index > 0 ? "12px" : "5px", width: "100%" }}
         >
           <div>
             <div
@@ -108,7 +128,7 @@ const Experience = (props: Props) => {
             <h3 className={style.infoText}>{project.description}</h3>
             <h3 className={style.date}>{project.date}</h3>
           </div>
-          <div style={{ float: 'right', marginLeft: 'auto', marginRight: '0' }}>
+          <div style={{ float: "right", marginLeft: "auto", marginRight: "0" }}>
             {project.link ? (
               <>
                 <a target="_blank" href={project.link}>
@@ -122,7 +142,7 @@ const Experience = (props: Props) => {
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,43 +1,57 @@
-import React from 'react'
-import style from "./Skills.module.css"
+import React from "react";
+import style from "./Skills.module.css";
 
-type Props = {}
+type Props = {};
 
 const Skills = (props: Props) => {
   return (
     <div>
       <h1>Skills</h1>
-      <div className='rowContainer'>
-        <div style={{width:"33%"}}>
+      <div className="rowContainer">
+        <div style={{ width: "33%" }}>
           <h2>Primary</h2>
-          <div className='rowContainer' id="skillOut">
-            <div className={style.accent} style={{backgroundColor: "#8BE9FE"}} id="accent"></div>
-            <div style={{marginTop:"3px"}}>
+          <div className="rowContainer" id="skillOut">
+            <div
+              className={style.accent}
+              style={{ backgroundColor: "#8BE9FE" }}
+              id="accent"
+            ></div>
+            <div style={{ marginTop: "3px" }}>
               <h3>Web3</h3>
               <h3>React</h3>
-              <h3>Java Script</h3>
+              <h3>Type Script</h3>
               <h3>UI Design</h3>
+              <h3>
+                Solidity <span className={style.learning}>Learning</span>
+              </h3>
             </div>
           </div>
         </div>
-        <div style={{width:"33%"}}>
+        <div style={{ width: "33%" }}>
           <h2>Secondary</h2>
-          <div className='rowContainer' id="skillOut">
-          <div className={style.accent} style={{backgroundColor: "#76CEFF"}} id="accent"></div>
-            <div style={{marginTop:"3px"}}>
+          <div className="rowContainer" id="skillOut">
+            <div
+              className={style.accent}
+              style={{ backgroundColor: "#76CEFF" }}
+              id="accent"
+            ></div>
+            <div style={{ marginTop: "3px" }}>
               <h3>Python</h3>
               <h3>Java</h3>
               <h3>SQL</h3>
               <h3>Node.js</h3>
-              <h3>Solidity <span className={style.learning}>Learning</span></h3>
             </div>
           </div>
         </div>
-        <div style={{width:"33%"}}>
+        <div style={{ width: "33%" }}>
           <h2>Extras</h2>
-          <div className='rowContainer' id="skillOut">
-            <div className={style.accent} style={{backgroundColor: "#41AFFE"}}  id="accent"></div>
-            <div style={{marginTop:"3px"}}>
+          <div className="rowContainer" id="skillOut">
+            <div
+              className={style.accent}
+              style={{ backgroundColor: "#41AFFE" }}
+              id="accent"
+            ></div>
+            <div style={{ marginTop: "3px" }}>
               <h3>3D Modeling</h3>
               <h3>3D Printing</h3>
               <h3>Video Editing</h3>
@@ -47,7 +61,7 @@ const Skills = (props: Props) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
